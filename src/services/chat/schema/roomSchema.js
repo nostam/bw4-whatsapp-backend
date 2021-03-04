1afdaa77eacf8b7e8356b953f09ae5c107712d59
nostam <matsonho@gmail.com>
2021-03-04 18:03:50 +0100

    update room schema and creating room 1o1

--- src/services/chat/schema/roomSchema.js
+++ src/services/chat/schema/roomSchema.js
@@ -1,31 +1,33 @@
 const { Schema, model } = require("mongoose");
-const RoomSchema = new Schema({
-  roomName: {
-    type: String,
-    required: true,
-  },
-  avatar: {
-    type: String,
-    default:
-      "http://getdrawings.com/free-icon-bw/best-group-icon-for-whatsapp-6.png",
-  },
-  creator: { type: Schema.Types.ObjectId, ref: "users" },
-  admins: [{ type: Schema.Types.ObjectId, ref: "users" }],
-  isGroup: {
-    type: Boolean,
-    required: true,
-  },
-  members: [{ type: Schema.Types.ObjectId, ref: "users" }],
-  messages: [
-    {
-      text: { type: String },
-      sender: { type: Schema.Types.ObjectId, ref: "users" },
-      createdAt: { type: Date },
+const RoomSchema = new Schema(
+  {
+    roomName: {
+      type: String,
+      required: true,
     },
-    {
-      timestamps: true,
+    avatar: {
+      type: String,
+      default:
+        "http://getdrawings.com/free-icon-bw/best-group-icon-for-whatsapp-6.png",
     },
-  ],
-});
+    creator: { type: Schema.Types.ObjectId, ref: "users" },
+    admins: [{ type: Schema.Types.ObjectId, ref: "users" }],
+    isGroup: {
+      type: Boolean,
+      required: true,
+    },
+    members: [{ type: Schema.Types.ObjectId, ref: "users" }],
+    messages: [
+      {
+        text: { type: String },
+        sender: { type: Schema.Types.ObjectId, ref: "users" },
+        createdAt: { type: Date },
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
+);
 
 module.exports = model("rooms", RoomSchema);
